c643e77d00618a0253634fc8bfe416d2cd22b90e
Antonio Terceiro <terceiro@softwarelivre.org>
2013-01-07 16:43:53 -0300

    Remove examples, add TODO and fix version number

--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(ruby2.0):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="ruby2.0" command="/usr/bin/ruby2.0"
--- debian/ruby2.0.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the ruby2.0 package
-#
-0 4	* * *	root	[ -x /usr/bin/ruby2.0_maintenance ] && /usr/bin/ruby2.0_maintenance
